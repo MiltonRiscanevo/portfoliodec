6531babe9817d96f444a900fbf3c92fe6b3888ff
Asistente Nomina FNC - Milton Riscanevo <mriscanevo@neumologica.org>
2022-12-26 16:33:44 -0500

    adicionando cambio al portfolio

---
+++ portfolio/src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import "../css/cssHeader.css"
+
+const Header = () => {
+  return (
+    <div className='title_contain'>
+        <h1 className='title_name'>MILTON RISCANEVO</h1>
+        <h2 className='title_position'>SOFTWARE DEVELOPER</h2>
+
+    </div>
+  )
+}
+
+export default Header
